45a6c25a3dd733a8ef24cafdcca7acc20e39cec5
DossTech <164689494+DossTech@users.noreply.github.com>
2025-02-20 23:41:01 +0530

    Update StringReverse.java

--- stringprograms/StringReverse.java
+++ stringprograms/StringReverse.java
@@ -1,12 +1,1 @@
-public class StringReverse{  
 
-	public static void main(String[] args) {
-
-		String s1 = "aabbcc";
-		
-		for (int i = s1.length()-1; i >=0 ; i--) {
-			char ca = s1.charAt(i);
-			System.out.print(ca);
-		}
-		
-	}}
